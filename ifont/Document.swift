12ca15f5d906ab46e8155ba02962421ecec181ad
undancer <undancer@gmail.com>
2018-09-23 11:38:30 +0800

    Initial Commit

---
+++ ifont/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  ifont
+//
+//  Created by undancer on 2018/9/23.
+//  Copyright © 2018 undancer. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
---
+++ ifont/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ifont
+//
+//  Created by undancer on 2018/9/23.
+//  Copyright © 2018 undancer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
